d507d8d11ad3b835fa2422bb79fc2665264b1777
abhii30 <akumar300599@gmail.com>
2023-04-07 13:06:32 +0530

    configure tailwind

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="m-2 text-9xl w-screen flex justify-center text-red-400">
+      Hello world!
+    </div>
+  );
 }
 
 export default App;
