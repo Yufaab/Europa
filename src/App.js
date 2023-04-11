b1107113633129c4b28f1d1f6c1956c60e1936a4
JazibEqbal <jazibeqbal0526@gmail.com>
2023-04-11 23:55:05 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterCouncelling from './components/RegisterCouncelling';
