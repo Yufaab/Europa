99f8d365454c3d472fa40bb7606751f872eeaff5
JazibEqbal <jazibeqbal0526@gmail.com>
2023-04-11 22:52:59 +0530

    Register Councelling Completed

--- src/components/RegisterCouncelling.js
+++ src/components/RegisterCouncelling.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import styles from './RegisterCouncelling.module.css';
 
 function RegisterCouncelling() {
   return (
-    <div>
-      <h3>Register Now To Consult A Councellor</h3>
-      <input type="text" placeholder="Full Name" />
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Mobile Number" />
-      <input type="text" placeholder="Address" />
-      <input type="text" placeholder="Rank" />
-      <textarea name="message" rows="2" cols="30" placeholder="Write a message">
-        {}
-      </textarea>
-      <button type="submit">Register Now</button>
+    <div className={styles.topContainerRegister}>
+      <h3 className={styles.councellingHead}>
+        Register Now To Consult A Councellor
+      </h3>
+      <form className={styles.councellingForm}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          className={styles.formInputs}
+        />
+        <input type="email" placeholder="Email" className={styles.formInputs} />
+        <input
+          type="text"
+          placeholder="Mobile Number"
+          className={styles.formInputs}
+        />
+        <input
+          type="text"
+          placeholder="City/State"
+          className={styles.formInputs}
+        />
+        <input type="text" placeholder="Rank" className={styles.formInputs} />
+        <textarea
+          name="message"
+          rows="2"
+          cols="30"
+          placeholder="Write a message"
+          className={`${styles.formInputsTextareaPlace} ${styles.formInputs}`}
+        >
+          {}
+        </textarea>
+        <button type="submit" className={styles.formCouncellingSubmitButton}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
