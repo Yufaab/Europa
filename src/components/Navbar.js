91ba59d446b48a01a73eb77311ca16b0edce47b2
abhishekTalocity <t-abhishek.kumar@talocity.ai>
2023-04-20 20:48:57 +0530

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,81 +7,79 @@ export default function NavBar() {
 
   return (
     <nav className="w-full bg-gradient-to-b from-black fixed top-0">
-      <div className="justify-between px-4 mx-auto lg:max-w-8xl md:items-center md:flex md:px-8 ">
-        <div>
-          <div className="flex items-center justify-between md:py-5 md:block">
-            <img src={Logo} alt="logo" className="relative my-[-20px]" />
-            <h2 className="absolute text-2xl font-medium text-[#fff] my-[-60px] mx-[90px]">
+      <div className="px-4 mx-auto lg:max-w-8xl md:items-center md:flex md:px-8">
+        <div className="flex items-center justify-between md:py-5 md:block">
+          <div className="w-[200px] relative">
+            <img src={Logo} alt="logo" className="relative md:my-[-20px]" />
+            <h2 className="absolute mx-auto left-0 right-0 text-2xl font-medium text-[#fff] top-3 text-center">
               Yufaab
             </h2>
-            <div className="md:hidden">
-              <button
-                type="button"
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => {
-                  setNavbar(!navbar);
-                }}
-              >
-                {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
+          </div>
+          <div className="md:hidden">
+            <button
+              type="button"
+              className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+              onClick={() => {
+                setNavbar(!navbar);
+              }}
+            >
+              {navbar ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 text-white"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
           </div>
         </div>
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? 'block' : 'hidden'
-            }`}
-          >
-            <ul className="items-center justify-center text-[18px] space-y-8 md:flex md:space-x-6 md:space-y-0">
-              <li className="text-white hover:text-[#0098FF]">
-                <a href="#">Home</a>
-              </li>
-              <li className="text-white hover:text-[#0098FF]">
-                <a href="#">About</a>
-              </li>
-              <li className="text-white hover:text-[#0098FF]">
-                <a href="#">Colleges</a>
-              </li>
-              <li className="text-white hover:text-[#0098FF]">
-                <a href="#">Rank</a>
-              </li>
-            </ul>
-          </div>
+        <div
+          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            navbar ? 'block' : 'hidden'
+          }`}
+        >
+          <ul className="items-center justify-center text-[18px] space-y-8 md:flex md:space-x-6 md:space-y-0">
+            <li className="text-white hover:text-[#0098FF]">
+              <a href="#">Home</a>
+            </li>
+            <li className="text-white hover:text-[#0098FF]">
+              <a href="#">About</a>
+            </li>
+            <li className="text-white hover:text-[#0098FF]">
+              <a href="#">Colleges</a>
+            </li>
+            <li className="text-white hover:text-[#0098FF]">
+              <a href="#">Rank</a>
+            </li>
+          </ul>
         </div>
-        <div className=" flex-row hidden  md:inline-block">
+        <div className=" w-[200px] hidden md:flex md:justify-end">
           {profile ? (
             <a
               href="#"
-              className="px-4 py-2 text-white bg-[#0098FF] rounded-md hover:bg-blue-600"
+              className=" px-4 py-2 text-white bg-[#0098FF] rounded-md hover:bg-blue-600"
               onClick={() => {
                 setProfile(!profile);
               }}
